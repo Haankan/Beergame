a0fe2e719fe399c153c0cf69b74eda263114f8b1
Hannes <gushannebj@student.gu.se>
2016-12-05 11:46:52 +0100

    ny view

--- app/src/main/java/com/example/hannes/beergame/common/CardDeck.java
+++ app/src/main/java/com/example/hannes/beergame/common/CardDeck.java
@@ -10,7 +10,7 @@ public class CardDeck {
 
     public CardDeck(String name, String email) {
         this.name = name;
-        this.email = email;
+        this.email = email;dgfkdjn
     }
 
     @Override

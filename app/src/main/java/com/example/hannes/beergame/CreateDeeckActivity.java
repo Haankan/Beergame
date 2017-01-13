976bfcf70e030b6e61f88205cb91f205e7111e14
Viktor <gusjosvic@student.gu.se>
2017-01-13 14:46:03 +0100

    Merge branch 'master' of https://github.com/Haankan/Beergame
    # Conflicts:
    #	app/src/main/java/com/example/hannes/beergame/CreateDeckActivity.java

--- app/src/main/java/com/example/hannes/beergame/CreateDeeckActivity.java
+++ app/src/main/java/com/example/hannes/beergame/CreateDeeckActivity.java
@@ -2,21 +2,40 @@ package com.example.hannes.beergame;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 public class CreateDeeckActivity extends AppCompatActivity {
+    private ArrayList<Integer> myImageList = new ArrayList<>();
 
+    getResources()
+    /*int[] myCardView = new int[]{R.drawable.h1, R.drawable.h2, R.drawable.h3, R.drawable.h4,
+            R.drawable.h5, R.drawable.h6, R.drawable.h7, R.drawable.h8, R.drawable.h9, R.drawable.h10,
+            R.drawable.h11, R.drawable.h12, R.drawable.h13, R.drawable.s14, R.drawable.s15, R.drawable.s16,
+            R.drawable.s17, R.drawable.s18, R.drawable.s19, R.drawable.s20, R.drawable.s21, R.drawable.s22,
+            R.drawable.s23, R.drawable.s24, R.drawable.s25, R.drawable.s26, R.drawable.r27, R.drawable.r28,
+            R.drawable.r29, R.drawable.r30, R.drawable.r31, R.drawable.r32, R.drawable.r33, R.drawable.r34,
+            R.drawable.r35, R.drawable.r36, R.drawable.r37, R.drawable.r38, R.drawable.r39, R.drawable.k40,
+            R.drawable.k41, R.drawable.k42, R.drawable.k43, R.drawable.k44, R.drawable.k45, R.drawable.k46,
+            R.drawable.k47, R.drawable.k48, R.drawable.k49, R.drawable.k50, R.drawable.k51, R.drawable.k52};
+
+    private String[] instructions;
+
+    imgView.setImageResource(myImageList[i]);*/
+    imgView.setImageResource(
+
+    getIdentifier("com.example.hannes.beergame:drawable/"+"kortrygg_tbg",null,null)
+
+    .
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_deeck);
+        final ImageView imgView = (ImageView) findViewById(R.id.kortvy);
     }
 
-    private String[] instructions;
-    private ArrayList<Integer> myImageList = new ArrayList<>();
-
-/*imgView.setImageResource(getResources().getIdentifier("com.example.hannes.beergame:drawable/" + "kortrygg_tbg", null, null));
+    );
         myImageList.add(R.drawable.h1);
         myImageList.add(R.drawable.h2);
         myImageList.add(R.drawable.h3);

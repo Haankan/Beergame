616ff838f86afe1e48f38499b12fa5fd99bef52b
Hannes <gushannebj@student.gu.se>
2017-01-13 12:05:30 +0100

    ny view

--- app/src/main/java/com/example/hannes/beergame/customizeActivity.java
+++ app/src/main/java/com/example/hannes/beergame/customizeActivity.java
@@ -1,7 +1,10 @@
 package com.example.hannes.beergame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 public class customizeActivity extends AppCompatActivity {
 
@@ -9,6 +12,19 @@ public class customizeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customize);
+        EditText text = (EditText) findViewById(R.id.editText3);
+        String leknamn = text.getText().toString();
+
+        if(TextUtils.isEmpty(leknamn)) {
+            text.setError("Your message");
+            return;
+
+
+                }else {
+                Intent intent = new Intent(customizeActivity.this, choosedeckActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
